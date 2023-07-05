92ab7812e3809379522f75100fba26ed6a6a0434
medicationforall <inklink28@gmail.com>
2023-07-05 15:50:37 -0400

    Don't repeat yourself.

--- src/cqdome/Dome.py
+++ src/cqdome/Dome.py
@@ -1,37 +1,33 @@
 import cadquery as cq
 from cqdome import Base
 
-def _make_pentagon(
-        pen_radius,
-        hex_height,
-        x_rotate=0,
-        z_rotate=0
-    ):
+def _make_pentagon(pen_radius, hex_height):
     pentagon = (
         cq.Workplane("XY")
         .polygon(5, pen_radius)
         .extrude(hex_height)
         .translate((0,0,-1*(hex_height/2)))
-        .rotate((0,0,1),(0,0,0),z_rotate)
-        .rotate((1,0,0),(0,0,0),x_rotate)
+
     )
     return pentagon
 
-def _make_hexagon(
-        hex_radius,
-        hex_height,
-        x_rotate=0,
-        z_rotate=0
-    ):
+def _make_hexagon(hex_radius, hex_height):
     hexagon = (
         cq.Workplane("XY")
         .polygon(6, hex_radius)
         .extrude(hex_height)
         .translate((0,0,-1*(hex_height/2)))
+    )
+    return hexagon
+
+def _rotate(shape, x_rotate=0, z_rotate=0):
+    shape = (
+        cq.Workplane("XY")
+        .union(shape) #clone the shape
         .rotate((0,0,1),(0,0,0),z_rotate)
         .rotate((1,0,0),(0,0,0),x_rotate)
     )
-    return hexagon
+    return shape
 
 class Dome(Base):
 
@@ -61,19 +57,10 @@ class Dome(Base):
         self.render_cut_keys = True
 
         #--- shapes
-        self.center_pentagon = None
-        self.center_pentagon_cut = None
-
-        self.r1_hex = None
-        self.r1_hex_cut = None
-
-        self.r2_hex = None
-        self.r2_hex_cut = None
-
-        self.r2_pen = None
-        self.r2_pen_cut = None
-
-        self.r3_hex = None
+        self.pentagon = None
+        self.pentagon_cut = None
+        self.hexagon = None
+        self.hexagon_cut = None
         self.box_cut = None
 
         self.hexagon_cut_key = None
@@ -82,84 +69,25 @@ class Dome(Base):
 
     def make(self):
         super().make()
+        self.pentagon = _make_pentagon(self.pen_radius, self.hex_height)
+        self.hexagon = _make_hexagon(self.hex_radius, self.hex_height)
 
-        self.__make_center()
-        self.__make_r1_shapes()
-        self.__make_r2_shapes()
-        self.__make_r3_shapes()
-        self.__make_box_cut()
+        self.pentagon_cut = _make_pentagon(
+            self.pen_radius - self.pen_radius_cut,
+            self.hex_height
+        )
+        self.hexagon_cut = _make_hexagon(
+            self.hex_radius - self.hex_radius_cut,
+            self.hex_height
+        )
+
+        self.box_cut = cq.Workplane("XY").box(153,160,150)
 
         if self.render_cut_keys:
             self.__make_hexagon_cut_key()
             self.__make_pentagon_cut_key()
 
 
-    def __make_center(self):
-        self.center_pentagon = _make_pentagon(
-            self.pen_radius,
-            self.hex_height
-        )
-
-        self.center_pentagon_cut = _make_pentagon(
-            self.pen_radius - self.pen_radius_cut,
-            self.hex_height
-        )
-
-
-    def __make_r1_shapes(self):
-        self.r1_hex = _make_hexagon(
-            self.hex_radius,
-            self.hex_height,
-            self.r1_x_rotate
-        )
-
-        self.r1_hex_cut = _make_hexagon(
-            self.hex_radius - self.hex_radius_cut,
-            self.hex_height,
-            self.r1_x_rotate
-        )
-
-
-    def __make_r2_shapes(self):
-        self.r2_hex = _make_hexagon(
-            self.hex_radius,
-            self.hex_height,
-            self.r2_x_rotate,
-            self.r2_z_rotate
-        )
-
-        self.r2_hex_cut = _make_hexagon(
-            self.hex_radius - self.hex_radius_cut,
-            self.hex_height,
-            self.r2_x_rotate,
-            self.r2_z_rotate
-        )
-
-        self.r2_pen = _make_pentagon(
-            self.pen_radius,
-            self.hex_height,
-            self.r2_pen_x_rotate,
-            self.r2_pen_z_rotate
-        )
-
-        self.r2_pen_cut = _make_pentagon(
-            self.pen_radius - self.pen_radius_cut,
-            self.hex_height,
-            self.r2_pen_x_rotate,
-            self.r2_pen_z_rotate
-        )
-
-    def __make_r3_shapes(self):
-        self.r3_hex = _make_hexagon(
-            self.hex_radius,
-            self.hex_height,
-            self.r3_x_rotate,
-            self.r3_z_rotate
-        )
-
-
-    def __make_box_cut(self):
-        self.box_cut = cq.Workplane("XY").box(153,160,150)
 
     def __make_hexagon_cut_key(self):
         hexagon_cut = _make_hexagon(
@@ -196,103 +124,14 @@ class Dome(Base):
 
     def build(self):
         super().build()
-        '''
-        dome = (
-            cq.Workplane("XY")
-            .union(self.center_pentagon)
-            .cut(self.center_pentagon_cut)
-
-            .union(self.r1_hex)
-            .cut(self.r1_hex_cut)
-
-            .union(self.r2_hex)
-            .cut(self.r2_hex_cut)
-
-            .union(self.r2_pen)
-            .cut(self.r2_pen_cut)
-
-            .union(self.r3_hex)
-            .cut(self.r3_hex_cut)
-
-            #.union(self.box_cut)
-        )
-        '''
 
         dome = cq.Workplane("XY")
 
-        solid_dome = (
-            cq.Workplane("XY")
-            #center
-            .union(self.center_pentagon.translate((0,0,12.2)).rotate((0,0,1),(0,0,0),18))
-
-            #ring 1
-            .union(self.r1_hex.translate((0,38.5,0))) #right
-            .union(self.r1_hex.translate((0,38.5,0)).rotate((0,0,1),(0,0,0), 72))
-            .union(self.r1_hex.translate((0,38.5,0)).rotate((0,0,1),(0,0,0), 72*2))
-            .union(self.r1_hex.translate((0,38.5,0)).rotate((0,0,1),(0,0,0), 72*3))
-            .union(self.r1_hex.translate((0,38.5,0)).rotate((0,0,1),(0,0,0), 72*4))
-        )
-
-        cut_dome = (
-            cq.Workplane("XY")
-            #center
-            .union(self.center_pentagon_cut.translate((0,0,12.2)).rotate((0,0,1),(0,0,0),18))
-
-            #ring 1
-            .union(self.r1_hex_cut.translate((0,38.5,0))) #right
-            .union(self.r1_hex_cut.translate((0,38.5,0)).rotate((0,0,1),(0,0,0), 72))
-            .union(self.r1_hex_cut.translate((0,38.5,0)).rotate((0,0,1),(0,0,0), 72*2))
-            .union(self.r1_hex_cut.translate((0,38.5,0)).rotate((0,0,1),(0,0,0), 72*3))
-            .union(self.r1_hex_cut.translate((0,38.5,0)).rotate((0,0,1),(0,0,0), 72*4))
-        )
-
-        r2_p_y = 65.6
-        ring_2 = (
-            cq.Workplane("XY")
-            .union(self.r2_hex.translate((0,-61.5,-23)))
-            .union(self.r2_hex.translate((0,-61.5,-23)).rotate((0,0,1),(0,0,0),72))
-            .union(self.r2_hex.translate((0,-61.5,-23)).rotate((0,0,1),(0,0,0),72*2))
-            .union(self.r2_hex.translate((0,-61.5,-23)).rotate((0,0,1),(0,0,0),72*3))
-            .union(self.r2_hex.translate((0,-61.5,-23)).rotate((0,0,1),(0,0,0),72*4))
-
-            .union(self.r2_pen.translate((0,r2_p_y,-28.3)))
-            .union(self.r2_pen.translate((0,r2_p_y,-28.3)).rotate((0,0,1),(0,0,0),72))
-            .union(self.r2_pen.translate((0,r2_p_y,-28.3)).rotate((0,0,1),(0,0,0),72*2))
-            .union(self.r2_pen.translate((0,r2_p_y,-28.3)).rotate((0,0,1),(0,0,0),72*3))
-            .union(self.r2_pen.translate((0,r2_p_y,-28.3)).rotate((0,0,1),(0,0,0),72*4))
-        )
-
-        ring_2_cut = (
-            cq.Workplane("XY")
-            .union(self.r2_hex_cut.translate((0,-61.5,-23)))
-            .union(self.r2_hex_cut.translate((0,-61.5,-23)).rotate((0,0,1),(0,0,0),72))
-            .union(self.r2_hex_cut.translate((0,-61.5,-23)).rotate((0,0,1),(0,0,0),72*2))
-            .union(self.r2_hex_cut.translate((0,-61.5,-23)).rotate((0,0,1),(0,0,0),72*3))
-            .union(self.r2_hex_cut.translate((0,-61.5,-23)).rotate((0,0,1),(0,0,0),72*4))
-
-            .union(self.r2_pen_cut.translate((0,r2_p_y,-28.3)))
-            .union(self.r2_pen_cut.translate((0,r2_p_y,-28.3)).rotate((0,0,1),(0,0,0),72))
-            .union(self.r2_pen_cut.translate((0,r2_p_y,-28.3)).rotate((0,0,1),(0,0,0),72*2))
-            .union(self.r2_pen_cut.translate((0,r2_p_y,-28.3)).rotate((0,0,1),(0,0,0),72*3))
-            .union(self.r2_pen_cut.translate((0,r2_p_y,-28.3)).rotate((0,0,1),(0,0,0),72*4))
-        )
-
-        r3_y=-73
-        r3_z=-1
-        ring_3 = (
-            cq.Workplane("XY")
-            .union(self.r3_hex.translate((0,r3_y,r3_z)))
-            .union(self.r3_hex.translate((0,r3_y,r3_z)).rotate((0,0,1),(0,0,0),36))
-            .union(self.r3_hex.translate((0,r3_y,r3_z)).rotate((0,0,1),(0,0,0),36*2))
-            .union(self.r3_hex.translate((0,r3_y,r3_z)).rotate((0,0,1),(0,0,0),36*3))
-            .union(self.r3_hex.translate((0,r3_y,r3_z)).rotate((0,0,1),(0,0,0),36*4))
-            .union(self.r3_hex.translate((0,r3_y,r3_z)).rotate((0,0,1),(0,0,0),36*5))
-            .union(self.r3_hex.translate((0,r3_y,r3_z)).rotate((0,0,1),(0,0,0),36*6))
-            .union(self.r3_hex.translate((0,r3_y,r3_z)).rotate((0,0,1),(0,0,0),36*7))
-            .union(self.r3_hex.translate((0,r3_y,r3_z)).rotate((0,0,1),(0,0,0),36*8))
-            .union(self.r3_hex.translate((0,r3_y,r3_z)).rotate((0,0,1),(0,0,0),36*9))
-        )
-
+        solid_dome = self.__build_ring1(self.hexagon, self.pentagon)
+        cut_dome = self.__build_ring1(self.hexagon_cut, self.pentagon_cut)
+        ring_2 = self.__build_ring2(self.hexagon, self.pentagon)
+        ring_2_cut = self.__build_ring2(self.hexagon_cut, self.pentagon_cut)
+        ring_3 = self.__build_ring_3(self.hexagon)
 
         dome = (
             dome
@@ -316,4 +155,78 @@ class Dome(Base):
             )
 
         return dome
-    
+
+    def __build_ring1(self, hex_shape, pen_shape):
+        h =  _rotate(hex_shape,self.r1_x_rotate,0)
+        p =  _rotate(pen_shape,0,0)
+        dome = (
+            cq.Workplane("XY")
+            #center
+            .union(
+                p
+                .translate((0,0,12.2))
+                .rotate((0,0,1),(0,0,0),18)
+            )
+        )
+
+        #ring 1
+        for i in range(5):
+            dome = (
+                dome
+                .union(
+                    h
+                    .translate((0,38.5,0))
+                    .rotate((0,0,1),(0,0,0), 72*i)
+                )
+            )
+
+        return dome
+
+
+    def __build_ring2(self, hex_shape, pen_shape):
+        h =  _rotate(hex_shape,self.r2_x_rotate,self.r2_z_rotate)
+        p =  _rotate(pen_shape,self.r2_pen_x_rotate,self.r2_pen_z_rotate)
+        r2_p_y = 65.6
+        ring = (
+            cq.Workplane("XY")
+        )
+
+        for i in range(5):
+            ring = (
+                ring
+                .union(
+                    h
+                    .translate((0,-61.5,-23))
+                    .rotate((0,0,1),(0,0,0),72*i)
+                )
+            )
+
+        for i in range(5):
+            ring = (
+                ring
+                .union(
+                    p
+                    .translate((0,r2_p_y,-28.3))
+                    .rotate((0,0,1),(0,0,0),72*i)
+                )
+            )
+
+        return ring
+
+
+    def __build_ring_3(self, hex_shape):
+        h =  _rotate(hex_shape,self.r3_x_rotate,self.r3_z_rotate)
+        r3_y=-73
+        r3_z=-1
+        ring_3 = (cq.Workplane("XY"))
+
+        for i in range(10):
+            ring_3 = (
+                ring_3
+                .union(
+                    h
+                    .translate((0,r3_y,r3_z))
+                    .rotate((0,0,1),(0,0,0),36*i)
+                )
+            )
+        return ring_3
